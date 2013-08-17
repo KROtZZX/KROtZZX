746abfc6d50d3fb8aa095211f91329f385c7afad
KROtZZX <aleksey.krotzzx@gmail.com>
2013-08-17 18:15:20 +0500

    Test180

---
+++ 180days/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>180 Дней Программирования</title>
+<link rel="stylesheet" href="style.css" type="text/css" />
+</head>
+
+<body>
+
+<div id="top_container">
+
+<a href="mailto:aleksey.krotz@gmail.com" title="Написать письмо"><img id="mail" src="img/mail.png"></a>
+
+<h1>Привет!</h1>
+
+<div id="string">Я, Алексей Ильин. Моя цель - повторить идею проекта 
+	<a href="http://jenniferdewalt.com/index.html">"180 сайтов за 180 дней"</a>.</div>
+<div id="string">Почитать о моих находках и велосипедах можно <a href="http://krotzzx.tumblr.com/">здесь</a>.</div>
+
+</div>
+
+<div id="marquee_container">
+    <div>Сегодня я выполнил</div>
+    <a href="http://180days.freeserver.me/montyholl/mholl.html">Проблема Монти Холла</a>
+</div>
+
+<div id="myworklist">
+
+
+
+
+
+<div>День 2 <span><a href="http://180days.freeserver.me/montyholl/mholl.html">Проблема Монти Холла</a></span></div>
+
+<div>День 1 <span><a href="http://180days.freeserver.me/">Домашняя страница</a></span></div>
+</div>
+</body>
+
+</html>
+
